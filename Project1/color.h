b87c1f16151e194e2d6e71c4de9d8df3ea8d232d
shibatachun <q1093677764@gmail.com>
2025-03-31 18:36:47 -0400

    Add project files.

---
+++ Project1/color.h
@@ -0,0 +1,27 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+#include "vec3.h"
+#include <iostream>
+#include <vector>
+
+using color = vec3;
+
+void write_color(std::vector<unsigned char>& image_data, const color& pixel_color, int i, int j, int image_width, int image_height, int channels )
+{
+	auto r = pixel_color.x();
+	auto g = pixel_color.y();
+	auto b = pixel_color.z();
+
+	int rbyte = int(255.999 * r);
+	int gbyte = int(255.999 * g);
+	int bbyte = int(255.999 * b);
+
+	int pixel_index = (j * image_width + i) * channels;
+	image_data[pixel_index + 0] = rbyte;
+	image_data[pixel_index + 1] = gbyte;
+	image_data[pixel_index + 2] = bbyte;
+
+}
+
+#endif
---
+++ Project1/main.cpp
@@ -0,0 +1,62 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+#include <vector>
+#include "color.h"
+#include "vec3.h"
+#include "ray.h"
+
+color ray_color(const ray& r)
+{
+	vec3 unit_direction = unit_vector(r.direction());
+	auto a = 0.5 * (unit_direction.y() + 1.0);
+	return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
+}
+int main()
+{
+	auto aspect_ratio = 16.0 / 9.0;
+	int image_width = 400;
+
+	int image_heigth = int(image_width / aspect_ratio);
+	image_heigth = (image_heigth < 1) ? 1 : image_heigth;
+	const int channels = 3;
+
+	//Camera
+	auto focal_length = 1.0;
+	auto viewport_height = 2.0;
+	auto viewport_width = viewport_height * (double(image_heigth) / image_heigth);
+	auto camera_center = point3(0, 0, 0);
+
+	//计算视口中的向量，用于划分视口中的像素
+	auto viewport_u = vec3(viewport_width, 0, 0);
+	auto viewport_v = vec3(0, -viewport_height, 0);
+
+	//划分像素，把视口划分成一格一格相等的像素格
+	auto pixel_delta_u = viewport_u / image_width;
+	auto pixel_delta_v = viewport_v / image_heigth;
+
+	auto viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
+	auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
+
+	std::vector<unsigned char> image_data(image_width * image_heigth * channels);
+
+	for (int j = 0; j < image_heigth; j++)
+	{
+		std::clog << "\rScanlines remaining: " << (image_heigth - j) << ' ' << std::flush;
+		for (int i = 0; i < image_width; i++)
+		{
+			auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+			auto ray_direction = pixel_center - camera_center;
+			ray r(camera_center, ray_direction);
+			
+			color pixel_color = ray_color(r);
+			write_color(image_data, pixel_color, i, j, image_width, image_heigth, channels);
+			
+		}
+	}
+	std::clog << "\rDone.                        \n";
+	stbi_write_png("image.png", image_width, image_heigth, channels, image_data.data(), image_width * channels);
+	std::cout << "Image saved to output.png\n";
+	return 0;
+}
