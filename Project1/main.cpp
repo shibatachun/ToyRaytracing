816fbff9e382af75c1bd5229a995f53a34971b4d
shibatachun <q1093677764@gmail.com>
2025-04-01 15:40:46 -0400

    update some classes for draw the sphere

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -7,8 +7,31 @@
 #include "vec3.h"
 #include "ray.h"
 
+double hit_sphere(const point3& center, double radius, const ray& r)
+{
+	vec3 oc = center - r.origin();
+	auto a = dot(r.direction(), r.direction());
+	auto h =dot(r.direction(), oc);
+	auto c = dot(oc, oc) - radius * radius;
+	auto discriminant = h*h - a * c;
+	if (discriminant < 0)
+	{
+		return -1.0;
+	}
+	else
+	{
+		return (h - std::sqrt(discriminant)) / a;
+	}
+	
+}
 color ray_color(const ray& r)
 {
+	auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
+	if (t > 0.0)
+	{
+		vec3 N = unit_vector(r.at(t) - vec3(0, 0, -1));
+		return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
+	}
 	vec3 unit_direction = unit_vector(r.direction());
 	auto a = 0.5 * (unit_direction.y() + 1.0);
 	return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
@@ -16,7 +39,7 @@ color ray_color(const ray& r)
 int main()
 {
 	auto aspect_ratio = 16.0 / 9.0;
-	int image_width = 400;
+	int  image_width = 400;
 
 	int image_heigth = int(image_width / aspect_ratio);
 	image_heigth = (image_heigth < 1) ? 1 : image_heigth;
@@ -25,7 +48,7 @@ int main()
 	//Camera
 	auto focal_length = 1.0;
 	auto viewport_height = 2.0;
-	auto viewport_width = viewport_height * (double(image_heigth) / image_heigth);
+	auto viewport_width = viewport_height * (double(image_width) / image_heigth);
 	auto camera_center = point3(0, 0, 0);
 
 	//计算视口中的向量，用于划分视口中的像素
